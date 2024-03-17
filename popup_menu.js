2f99c438dcd55d84f0502bbe571e6961ecafb62c
yymin1022 <yymin1022@gmail.com>
2024-03-18 01:50:57 +0900

    Update popup_menu.js : Added Gemini API Call Fetching

--- popup_menu.js
+++ popup_menu.js
@@ -1,11 +1,29 @@
+const GEMINI_API_KEY = "";
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`;
+
 let btnGemini = document.getElementById("btn_gemini");
 let inputText = document.getElementById("input_text");
 
 btnGemini.addEventListener("click", () => {
     btnGemini.disabled = true;
-    setTimeout(() => {
-        console.log(`Text is ${inputText.value}`);
-        console.log("Loading Done!");
-        btnGemini.disabled = false;
-    }, 3000);
+
+    fetch(GEMINI_API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            contents: [{
+                parts:[{
+                    text: inputText.value
+                }]
+            }]
+        }),
+    })
+    .then((response) => {
+        response.json();
+    })
+    .then((result) => {
+        console.log(result["candidates"][0]["content"]["parts"][0]["text"])
+    });
 });
